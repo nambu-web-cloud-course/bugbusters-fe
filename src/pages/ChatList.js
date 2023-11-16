4c1aba52cfb70ed330cfe50e04010f71835f1c55
yejwoo <yejinwoo.me@gmail.com>
2023-11-16 17:26:15 +0900

    Feat: Chat List

--- src/pages/ChatList.js
+++ src/pages/ChatList.js
@@ -1,3 +1,52 @@
+import axios from "axios";
+import Container from "../components/common/Container";
+import UserInfo from "../components/common/UserInfo";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Span } from "../components/common/Text";
+
 export default function ChatList() {
-  return <div>채팅리스트</div>;
+  const userid = JSON.parse(localStorage.getItem("userid"));
+  const usertype = JSON.parse(localStorage.getItem("usertype"));
+
+  const [chatroom, setChatRoom] = useState([]);
+
+  const getChatRoom = async () => {
+    const typeid = usertype === "B" ? "busterid" : "userid";
+    const URL = `/chat?${typeid}=${userid}`;
+
+    try {
+      const res = await axios.get(URL);
+      const data = res.data.data;
+      if (res.data.success) {
+        setChatRoom(data);
+      }
+    } catch (err) {
+      console.log("Get Chatroom Error", err);
+    }
+  };
+  useEffect(() => {
+    getChatRoom();
+  }, []);
+  console.log(chatroom);
+  
+  return (
+    <div className="Content">
+      <h1>채팅</h1>
+      {/* 내 아이디가 속한 채팅방 목록을 보여줌 */}
+      {chatroom.map((room) => (
+        <Link to={`/chat/${room.room}`}>
+          <Container>
+            <UserInfo
+              key={room.room}
+              busterid={room.busterid}
+              userid={room.userid}
+              usertype={usertype}
+            />
+            <Span>{room.createdAt}</Span>
+          </Container>
+        </Link>
+      ))}
+    </div>
+  );
 }
